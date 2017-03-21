a385d420801ab7d4017237c76713e1c133d50ec4
rhowlett <rhowlett@nilar.com>
2017-03-21 10:18:03 -0600

    Added full hierarchy
    Pushes down from the top into each schematic to see if there are additional schematics that need formatting.

--- kicad_sch_standardize.sh
+++ kicad_sch_standardize.sh
@@ -1,19 +1,43 @@
 #!/usr/bin/env bash
+temp_file=/tmp/temp_list.txt
+rm -f $temp_file
 
 if [ -s $1.sch ]
 then
-  echo $1.sch >schematic_list.txt
-  kicad_sch_page_order.pl  -schem -file $1.sch >>schematic_list.txt
-  extension=$(date +%Y%j%H%M%S)
-  for sch in $(cat schematic_list.txt)
+  echo $1.sch >$temp_file
+  kicad_sch_page_order.pl  -schem -file $1.sch >>$temp_file
+
+  sort -u $temp_file >$temp_file.1
+  mv $temp_file.1 $temp_file
+  word_count=$(cat $temp_file |wc -l)
+  diff_count=$word_count
+  while [ $diff_count -ge 1 ]
   do
-    cp $sch $extension.$sch
-    echo "backing up orignal file from $sch to $extension.$sch"
-    kicad_sch_font_resize.pl -verbose -sheet 50 -sch 50 -pin 50 -glabel 40 -hlabel 50 -wlabel 40 -power 30 -ref 40 -value 40 -footprint 30 -file $sch 1>temp.$sch 2>$sch.log
-    mv temp.$sch $sch
+    for sch in $(cat $temp_file)
+    do
+      last_count=$word_count
+      kicad_sch_page_order.pl  -schem -file $sch >>$temp_file
+      sort -u $temp_file >$temp_file.1
+      mv $temp_file.1 $temp_file
+      word_count=$(cat $temp_file |wc -l)
+      let diff_count=$word_count-$last_count
+      #echo "last = $last_count , count = $word_count , diff = $diff_count"
+    done
   done
 else
   print "Error - need a schematic or project name without the (.sch or .pro) extention"
   print "USAGE - $0 <project_name>"
+  exit 1
 fi
-exit
+
+extension=$(date +%Y%j%H%M%S)
+mkdir backup_$extension
+for sch in $(cat $temp_file)
+do
+  cp $sch backup_$extension/.
+  echo "backing up orignal file from $sch to backup_$extension/$sch"
+  kicad_sch_font_resize.pl -verbose -sheet 50 -sch 50 -pin 50 -glabel 40 -hlabel 50 -wlabel 40 -power 30 -ref 40 -value 40 -footprint 30 -file $sch 1>temp.$sch 2>$sch.log
+  mv temp.$sch $sch
+done
+
+exit 0
